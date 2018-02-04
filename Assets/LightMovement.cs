a46ffc8b60ce25bf9427aed18f17bc4a0fc999c8
Shraeyas <shraeyassingh@gmail.com>
2018-02-04 19:05:36 +0530

    Physics

---
+++ Assets/LightMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class LightMovement : MonoBehaviour 
+{
+	public Transform cube;
+	public Transform light;
+	public Vector3 offset;
+
+	void Start () 
+	{
+
+	}
+
+	void FixedUpdate () 
+	{
+		light.position = cube.position + offset;
+	}
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour 
+{
+	public Rigidbody rb;
+	public Transform camera;
+	public Vector3 offset;
+
+	void Start () 
+	{
+		
+	}
+
+	void FixedUpdate () 
+	{
+		rb.AddForce (0, -5, 15);
+
+		camera.position = transform.position + offset;
+	}
+}
